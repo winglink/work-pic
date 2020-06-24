25094697cd8884993e11121363dc7d4ae72fe02c
ww <zyq00123@icloud.com>
2020-06-24 11:32:04 +0800

    initial

---
+++ cmake-build-debug/CMakeFiles/pic.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pic.dir/main.cpp.obj"
+  "libpic.dll.a"
+  "pic.exe"
+  "pic.exe.manifest"
+  "pic.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
